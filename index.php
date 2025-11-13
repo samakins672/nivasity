ca976c4d928e7441b64a5228684ada439ea0d918
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2025-11-13 10:39:31 +0100

    updated flat account

--- index.php
+++ index.php
@@ -792,7 +792,7 @@ $show_store = (isset($_SESSION['nivas_userRole']) && $_SESSION['nivas_userRole']
         for (const seller in sellerTotals) {
             subaccounts.push({
                 id: seller,
-                transaction_charge_type: "flat",
+                transaction_charge_type: "flat_subaccount",
                 transaction_charge: sellerTotals[seller]
             });
         }
