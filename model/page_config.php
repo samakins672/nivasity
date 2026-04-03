720d8dedc539703b954b87111f99ed30fcb59e20
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2026-04-03 07:54:35 +0100

    feat: Add application domain and URL helper functions for improved URL management

--- model/page_config.php
+++ model/page_config.php
@@ -1,4 +1,39 @@
 <?php
+require_once __DIR__ . '/../config/db.php';
+
+if (!defined('APP_SCHOOL_ID')) {
+  define('APP_SCHOOL_ID', 1);
+}
+
+if (!defined('APP_DOMAIN')) {
+  define('APP_DOMAIN', 'https://funaab.nivasity.com');
+}
+
+if (!function_exists('nivasity_app_domain')) {
+  function nivasity_app_domain() {
+    return rtrim(APP_DOMAIN, '/');
+  }
+}
+
+if (!function_exists('nivasity_app_url')) {
+  function nivasity_app_url($path = '') {
+    $baseUrl = nivasity_app_domain();
+    $normalizedPath = ltrim((string) $path, '/');
+
+    if ($normalizedPath === '') {
+      return $baseUrl;
+    }
+
+    return $baseUrl . '/' . $normalizedPath;
+  }
+}
+
+if (!function_exists('nivasity_school_id')) {
+  function nivasity_school_id() {
+    return (int) APP_SCHOOL_ID;
+  }
+}
+
 require_once __DIR__ . '/../config/fw.php';
 require_once __DIR__ . '/mobile_experience_prompt.php';
 $url = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
